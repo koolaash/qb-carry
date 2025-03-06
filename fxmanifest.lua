727ff499ba849f8dbee9d7a3e7bae5e89830b5e8
Damon <73039811+koolaash@users.noreply.github.com>
2025-03-07 01:52:43 +0530

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+shared_script '@WaveShield/resource/waveshield.lua' --this line was automatically written by WaveShield
+
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'DDFW'
+description '3in1'
+version '1.0.0'
+
+client_script {
+    'cl_antivdm',
+    'cl_carry.lua',
+  }
+server_script {
+    "sv_carry.lua"
+}
